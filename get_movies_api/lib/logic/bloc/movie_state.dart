989880d776ee7ee2a1cc70eed33ec0c773497112
Mane <mane.ghudratyan@gmail.com>
2024-04-22 17:29:00 +0400

    Add fail

--- get_movies_api/lib/logic/bloc/movie_state.dart
+++ get_movies_api/lib/logic/bloc/movie_state.dart
@@ -1,9 +1,10 @@
 part of 'movie_bloc.dart';
 
 sealed class MovieState extends Equatable {
-  const MovieState({this.movies = const <MovieModel>[]});
+  const MovieState({this.movies = const <MovieModel>[], this.error});
 
   final List<MovieModel> movies;
+  final String? error;
 
   @override
   List<Object> get props => [movies];
@@ -18,4 +19,12 @@ final class MovieLoading extends MovieState {
 
 final class MovieLoaded extends MovieState {
   const MovieLoaded(List<MovieModel> models) : super(movies: models);
+}
+
+final class MovieFailed extends MovieState {
+  MovieFailed(MovieState initState, String error)
+      : super(
+          error: error,
+          movies: initState.movies,
+        );
 }
--- get_movies_api/lib/pages/home_page.dart
+++ get_movies_api/lib/pages/home_page.dart
@@ -5,7 +5,7 @@ import 'package:get_movies_api/services/api_service.dart';
 import 'package:get_movies_api/services/movie_service.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,18 +14,25 @@ class HomePage extends StatelessWidget {
         ..add(
           GetMovies(),
         ),
-      child: BlocBuilder<MovieBloc, MovieState>(
+      child: Scaffold(
+        body: BlocConsumer<MovieBloc, MovieState>(
+          listener: (context, state) {
+            if (state is MovieFailed && state.error != null) {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text(state.error!),
+              ));
+            }
+          },
           builder: (BuildContext context, MovieState state) {
-        return Scaffold(
-            appBar: AppBar(
-              title: const Text('Movies'),
-            ),
-            body: state is MovieLoading
+            return state is MovieLoading
                 ? const Center(
                     child: CircularProgressIndicator(),
                   )
-                : Center(child: Text(state.movies.toString())));
-      }),
+                : Center(child: Text(state.movies.toString()));
+          },
+        ),
+      ),
     );
   }
 }
+    
